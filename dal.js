5ed6d2640f1863a630bc043c8a0833bae4523269
Lakshmi Nizampatnam <151479712+LNizampatnam@users.noreply.github.com>
2024-09-19 20:47:03 -0500

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb+srv://lnizampatnam:JCR5py1NrK5695N1@cluster0.7lbgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url         = 'mongodb+srv://lnizampatnam:Mq7aGpeu8X24gLiz@cluster0.7lbgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 let db            = null;
  
 // connect to mongo
